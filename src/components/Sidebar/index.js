853b4f02e40ed3f6d6925256388749762512651b
brunogoniadis <brunogoniadislima@gmail.com>
2024-03-25 21:27:04 -0300

    feat: sidebar criada

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,37 @@
+import { mdiCalendarBlankOutline, mdiAccountMultiple } from '@mdi/js';
+import { Link, useLocation } from 'react-router-dom';
+
+import logo from "../../assets/Logo.svg.png";
+
 const Sidebar = () => {
+    const location = useLocation();
+
     return (
-        <h1>Sidebar</h1>
+        <sidebar className="col-2 h-100">
+            <img src={logo} className="img-fluid px-3 py-4" alt="Logo" />
+
+            <ul className='p-0 m-0'>
+                <li>
+                    <Link to="/" className={`mdi mdi-calendar-check ${location.pathname === '/' ? 'active' : ''}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                            <path d={mdiCalendarBlankOutline} fill="white" />
+                        </svg>
+                        <span>Agendamentos</span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/clientes" className={`mdi mdi-account ${location.pathname === '/clientes' ? 'active' : ''}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                            <path d={mdiAccountMultiple} fill="white" />
+                        </svg>
+                        <span>Clientes</span>
+
+                    </Link>
+                </li>
+            </ul>
+        </sidebar>
     )
 }
 
-export default Sidebar
+export default Sidebar;
--- src/pages/agendamentos/index.js
+++ src/pages/agendamentos/index.js
@@ -1,3 +1,5 @@
+import logo from "../../assets/Logo.svg.png"
+
 const Agendamentos = () => {
     return (
         <h1>Agendamentos</h1>
