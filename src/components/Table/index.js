1cd643444506ff1d6ec95a17a22733802064be19
brunogoniadis <brunogoniadislima@gmail.com>
2024-03-29 21:16:17 -0300

    feat: tabela de cliente criada

---
+++ src/components/Table/index.js
@@ -0,0 +1,40 @@
+const TableComponent = ({ data, config, actions, onRowClick }) => {
+    console.log('data', data);
+    console.log('config', config); // Log the received config for verification
+
+    return (
+        <div className="table-responsive max-height-500">
+            <table className="table table-striped table-bordered custom-table">
+                <colgroup>
+                    {config.map(({ width, fixed }, index) => (
+                        <col key={index} style={{ width: width, minWidth: width, maxWidth: width, position: fixed ? 'sticky' : 'static', left: fixed ? index * width : 'auto' }} />
+                    ))}
+                </colgroup>
+                <thead>
+                    <tr>
+                        {/* Render table headers based on the config */}
+                        {config.map(({ label }) => (
+                            <th key={label}>{label}</th>
+                        ))}
+                        {/* Render nova coluna */}
+                        <th key="novaColuna">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((cliente) => (
+                        <tr key={cliente.id} onClick={() => onRowClick(cliente)}> {/* Adicionando onClick para cada linha */}
+                            {/* Render table cells based on the config */}
+                            {config.map(({ key }) => (
+                                <td key={key}>{cliente[key]}</td>
+                            ))}
+                            {/* Renderizar o botão dentro da célula de ação */}
+                            <td key="novaColunaDado">{actions(cliente)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TableComponent;
